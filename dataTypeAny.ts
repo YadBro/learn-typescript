8d17bff550b11bf4d94af45a8af5788d0a373ad8
YadBro <ncada708@gmail.com>
2022-07-24 09:45:16 +0700

    Data Type (any)

---
+++ dataTypeAny.ts
@@ -0,0 +1,11 @@
+let anythingType: any = () => console.log('Hello Any');
+anythingType();
+anythingType = 'Not function again';
+// anythingType(); // this is error because anythingType variable overrided into string
+console.log(anythingType);
+anythingType = 5;
+console.log(anythingType);
+anythingType = true;
+console.log(anythingType);
+anythingType = false;
+console.log(anythingType);
