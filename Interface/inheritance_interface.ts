a7cef4e8bc159b1b95153db50199a1f5a96446a2
YadBro <ncada708@gmail.com>
2022-07-24 19:21:43 +0700

    Many extends for Interface

--- Interface/inheritance_interface.ts
+++ Interface/inheritance_interface.ts
@@ -15,3 +15,33 @@ let andri: HasPassword = {
 }
 
 console.log(andri);
+
+enum Gender {Male = 'Male', Female = 'Female'}
+
+interface StudentIndentity {
+  nik: number;
+}
+
+interface StudentCard {
+  numberCard: number
+}
+
+interface Students extends StudentIndentity, StudentCard {
+  name: string,
+  age: number,
+  gender: Gender,
+}
+
+// Mesti di ingat interface hanya bisa mengextends sebuah interface lainnya saja tidak dengan yang lain seperti enum dll.
+// Tetapi untuk property bisa di combine dengan enum
+
+let udin: Students = {
+  name: 'Udin Terpesona',
+  age: 15,
+  // gender: 'Male', // ini akan error karena tipe data gender sudah di combine dengan enum Gender
+  gender: Gender.Male, // success
+  nik: 213123,
+  numberCard: 123213,
+}
+
+console.log(udin);
